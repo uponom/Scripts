21f3e793e6cd4e3641b39bafc91641d8607c7990
Yurii Ponomarenko <ponomy@gmail.com>
2022-04-28 18:22:22 +0200

    Update pingp.ps1

--- Network/pingp.ps1
+++ Network/pingp.ps1
@@ -1,4 +1,32 @@
-﻿[cmdletbinding(
+﻿<#
+.SYNOPSIS
+    Network connectivity test to a specified host
+.DESCRIPTION
+    This scrips is wrapper for Test-NetConnection cmdlet to provide ping-like functionality. It can check network connectivity by simple ping or by connecting to TCP port. 
+.PARAMETER ComputerName
+    Remote host
+.PARAMETER CommonTCPPort
+    Protocol to test by TCP connection. It can be SMB, HTTP, RDP or WINRM. Simple ping will be used if parameter omitted.
+
+.PARAMETER SleepSeconds
+    Time interval between tests.
+
+.PARAMETER Repeat
+    How many times to repeat the test. Infinite repetition if the parameter omitted or 0.
+     
+.PARAMETER Warnings
+    Configures displaying of warnings messages. "SilentlyContinue" by default.    
+     
+.NOTES
+    Version:        0.1
+    Author:         Yurii Ponomarenko
+  
+.EXAMPLE
+    pingp.ps1 -ComputerName somehost.contoso.com CommonTCPPort RDP -SleepSeconds 3
+    Test network connectivity to somehost.contoso.com by TCP port 3389 in an infinite loop every 3 seconds.
+#>
+
+[cmdletbinding(
     SupportsShouldProcess,
     DefaultParameterSetName='Default'
 )]
@@ -11,6 +39,7 @@ param(
     
     [Parameter(Position=1,ValueFromPipelineByPropertyName=$true,ParameterSetName='Default')]
     [string]$CommonTCPPort = '',
+
     [int]$SleepSeconds = 15,
     [int]$Repeat = 0,
     [string]$Warnings = 'SilentlyContinue'
